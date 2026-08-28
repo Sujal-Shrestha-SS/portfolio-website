b65abc7e79d182540d2b85f037c1a6524f511450
Sujal-Shrestha-SS <sujalstha801@gmail.com>
2026-08-28 20:36:57 +0545

    Latest updates

--- portfolio/src/data/projectsData.js
+++ portfolio/src/data/projectsData.js
@@ -1,10 +1,5 @@
 const projects = [
-  {
-    title: "TaskCtrl",
-    image: "images/TaskCtrl.png",
-    link: "https://chelshrestha.free.nf",
-    description: "This is a todo app"
-  },
+ 
   {
     title: "CashFlow",
     image: "images/CashFlow.png",
@@ -18,12 +13,6 @@ const projects = [
     description: "This is a football club management system"
   },
   {
-    title: "RunTime",
-    image: "images/RunTime.png",
-    link: "https://runtimequiz.netlify.app",
-    description: "This is a web-quiz app"
-  },
-  {
     title: "StriveX",
     image: "images/StriveX.png",
     link: "https://strivex.rf.gd",
@@ -32,8 +21,20 @@ const projects = [
   {
     title: "Groovio",
     image: "images/Groovio.png",
-    link: "https://groovioo.netlify.app",
+    link: "https://groovioo.netlify.app/",
     description: "This is a music video player"
+  },
+  {
+    title: "Predictator",
+    image: "images/Predictator.png",
+    link: "https://predictatorr.vercel.app/",
+    description: "This is a FIFA World Cup 2026 Prediction Platform"
+  },
+   {
+    title: "InterviewIQ",
+    image: "images/InterviewIQ.png",
+    // link: "https://interviewiq.netlify.app",
+    description: "This is an intelligent interview preparation platform"
   }
 ]
 
